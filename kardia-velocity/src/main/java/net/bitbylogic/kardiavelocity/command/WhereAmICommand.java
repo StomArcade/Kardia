f28c23c31bafd8c93f9db6926dd9274437b968c4
Justin <33465177+BitByLogics@users.noreply.github.com>
2026-01-07 19:28:09 -0500

    Use `parsed` placeholders in `WhereAmICommand` to format server name with small caps.

--- kardia-velocity/src/main/java/net/bitbylogic/kardiavelocity/command/WhereAmICommand.java
+++ kardia-velocity/src/main/java/net/bitbylogic/kardiavelocity/command/WhereAmICommand.java
@@ -3,15 +3,16 @@ package net.bitbylogic.kardiavelocity.command;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
 import net.bitbylogic.kardiavelocity.util.message.MessageUtil;
+import net.bitbylogic.utils.smallcaps.SmallCapsConverter;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
 public class WhereAmICommand implements SimpleCommand {
 
     @Override
     public void execute(Invocation invocation) {
         if(invocation.source() instanceof Player sender) {
-            sender.sendMessage(MessageUtil.deserialize("<secondary>ʏᴏᴜ'ʀᴇ ᴄᴜʀʀᴇɴᴛʟʏ ᴏɴ ꜱᴇʀᴠᴇʀ <highlight>"
-                    + sender.getCurrentServer().get().getServerInfo().getName()));
+            sender.sendMessage(MessageUtil.deserialize("<secondary>ʏᴏᴜ'ʀᴇ ᴄᴜʀʀᴇɴᴛʟʏ ᴏɴ ꜱᴇʀᴠᴇʀ <highlight><server>", Placeholder.parsed("server", SmallCapsConverter.convert(sender.getCurrentServer().get().getServerInfo().getName()))));
         }
     }
 
